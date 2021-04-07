5c1a1e36fb00e809a007ec94e8b8df867e9acfe4
andresdavidsv <andresdavidsolartevidal@gmail.com>
2021-04-06 21:54:09 -0500

    :sparkles: [FIX] User name

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -26,13 +26,13 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
+          <a href="https://twitter.com/andresdavidsv">
             <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
+          <a href="https://github.com/andresdavidsv">
             <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
+          <a href="https://instagram.com/andresdavidsv">
             <img src="${instagram}" />
           </a>
         </div>
